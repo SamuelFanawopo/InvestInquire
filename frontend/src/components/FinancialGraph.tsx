e82a9c98123b2fce94786534e8b96102d5cf3eec
Samuel Fanawopo <sfanawopo@gmail.com>
2023-12-17 07:07:55 +0000

    changes

--- frontend/src/components/FinancialGraph.tsx
+++ frontend/src/components/FinancialGraph.tsx
@@ -1,20 +1,57 @@
 import React from "react";
+import { Line } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
 
-// Define the props for the component, if needed
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+);
+
 interface FinancialGraphProps {
   ticker: string;
 }
 
-// Define the FinancialGraph component
 const FinancialGraph: React.FC<FinancialGraphProps> = ({ ticker }) => {
-  // Component logic for setting up the graph goes here
+  const chartData = {
+    // Ensure labels are correctly formatted
+    labels: ["2023-12-15", "2023-12-14", "2023-12-13"],
+    datasets: [
+      {
+        label: `Stock Price for ${ticker}`,
+        data: [162.23, 162.91, 163.62],
+        fill: false,
+        borderColor: "rgb(75, 192, 192)",
+        tension: 0.1,
+      },
+    ],
+  };
+
+  const options = {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  };
 
   return (
     <div>
-      {/* Render your financial graph here */}
-      {/* For example, you might use a <LineChart> or <BarChart> component from a library */}
-      <h1>Financial Graph</h1>
-      <p>Ticker: {ticker}</p>
+      <h1>Financial Graph for {ticker}</h1>
+      <Line data={chartData} options={options} />
     </div>
   );
 };
