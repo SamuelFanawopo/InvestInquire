c524f87b99b8117774a01300642196cdd1d6ac93
Samuel Fanawopo <sfanawopo@gmail.com>
2023-12-18 22:20:57 +0000

    balance sheet component completed

--- frontend/src/components/FinancialGraph.tsx
+++ frontend/src/components/FinancialGraph.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Line } from "react-chartjs-2";
 import {
   Chart as ChartJS,
@@ -25,20 +25,47 @@ interface FinancialGraphProps {
   ticker: string;
 }
 
-const FinancialGraph: React.FC<FinancialGraphProps> = ({ ticker }) => {
-  const chartData = {
-    // Ensure labels are correctly formatted
-    labels: ["2023-12-15", "2023-12-14", "2023-12-13"],
-    datasets: [
-      {
-        label: `Stock Price for ${ticker}`,
-        data: [162.23, 162.91, 163.62],
-        fill: false,
-        borderColor: "rgb(75, 192, 192)",
-        tension: 0.1,
-      },
-    ],
+const FinancialGraph: React.FC<FinancialGraphProps> = ({
+  ticker,
+  monthlyData,
+}) => {
+  const [graphType, setGraphType] = useState("daily"); // 'daily', 'monthly', or 'ytd'
+
+  const getChartData = () => {
+    switch (graphType) {
+      case "daily":
+        return {
+          labels: ["2023-12-15", "2023-12-14", "2023-12-13"],
+          datasets: [
+            {
+              label: `Daily Stock Price for ${ticker}`,
+              data: [162.23, 162.91, 163.62],
+              fill: false,
+              borderColor: "rgb(75, 192, 192)",
+              tension: 0.1,
+            },
+          ],
+        };
+      case "monthly":
+        return {
+          labels: monthlyData.map((item) => item.date),
+          datasets: [
+            {
+              label: `Monthly Stock Price for ${ticker}`,
+              data: monthlyData.map((item) => item.value),
+              fill: false,
+              borderColor: "rgb(53, 162, 235)",
+              tension: 0.1,
+            },
+          ],
+        };
+      // Add case for 'ytd' if needed
+      default:
+        return {};
+    }
   };
+
+  const chartData = getChartData();
 
   const options = {
     scales: {
@@ -51,8 +78,13 @@ const FinancialGraph: React.FC<FinancialGraphProps> = ({ ticker }) => {
   return (
     <div>
       <h1 className="font-bold text-2xl text-center mb-5">
-        Daily Prices (open, high, low, close) and Volumes for {ticker}
+        Stock Prices for {ticker}
       </h1>
+      <div>
+        <button onClick={() => setGraphType("daily")}>Daily</button>
+        <button onClick={() => setGraphType("monthly")}>Monthly</button>
+        {/* Add button for 'ytd' if needed */}
+      </div>
       <Line data={chartData} options={options} />
     </div>
   );
--- frontend/src/slices/BalanceSheet.tsx
+++ frontend/src/slices/BalanceSheet.tsx
@@ -1,11 +1,91 @@
-const BalanceSheet = () => {
-  // You can add more logic or computations as needed
+import { useState } from "react";
+
+type BalanceSheetData = {
+  [key: string]: string | number;
+};
+
+const BalanceSheet: React.FC = () => {
+  const [showTable, setShowTable] = useState<boolean>(false);
+
+  const balanceSheetData: BalanceSheetData = {
+    fiscalDateEnding: "2022-12-31",
+    reportedCurrency: "USD",
+    totalAssets: "127243000000",
+    totalCurrentAssets: "29118000000",
+    cashAndCashEquivalentsAtCarryingValue: "7886000000",
+    cashAndShortTermInvestments: "7886000000",
+    inventory: "1552000000",
+    currentNetReceivables: "14209000000",
+    totalNonCurrentAssets: "96874000000",
+    propertyPlantEquipment: "5334000000",
+    accumulatedDepreciationAmortizationPPE: "13361000000",
+    intangibleAssets: "67133000000",
+    intangibleAssetsExcludingGoodwill: "11184000000",
+    goodwill: "55949000000",
+    investments: "None",
+    longTermInvestments: "142000000",
+    shortTermInvestments: "852000000",
+    otherCurrentAssets: "2610000000",
+    otherNonCurrentAssets: "None",
+    totalLiabilities: "105222000000",
+    totalCurrentLiabilities: "31505000000",
+    currentAccountsPayable: "4051000000",
+    deferredRevenue: "15531000000",
+    currentDebt: "9511000000",
+    shortTermDebt: "4760000000",
+    totalNonCurrentLiabilities: "83414000000",
+    capitalLeaseObligations: "164000000",
+    longTermDebt: "47190000000",
+    currentLongTermDebt: "4676000000",
+    longTermDebtNoncurrent: "46189000000",
+    shortLongTermDebtTotal: "107759000000",
+    otherCurrentLiabilities: "9788000000",
+    otherNonCurrentLiabilities: "12243000000",
+    totalShareholderEquity: "21944000000",
+    treasuryStock: "169484000000",
+    retainedEarnings: "149825000000",
+    commonStock: "58343000000",
+    commonStockSharesOutstanding: "906091977",
+  };
+
+  const renderTable = () => {
+    return (
+      <div className="overflow-x-auto mt-8">
+        <table className="min-w-full text-sm text-left text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                Category
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Value
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {Object.entries(balanceSheetData).map(([key, value]) => (
+              <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <td className="px-6 py-4">{key}</td>
+                <td className="px-6 py-4">{value}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  };
 
   return (
-    <div className="flex justify-center items-center">
-      <button className="bg-blue-500 hover:bg-blue-600 text-white rounded-lg py-2 px-6 transition duration-300 mt-6">
-        View Balance Sheet
-      </button>
+    <div className="flex flex-col items-center p-6">
+      <div className="w-full flex justify-center">
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline transition duration-300"
+          onClick={() => setShowTable(!showTable)}
+        >
+          {!showTable ? "View Balance Sheet" : "Close Balance Sheet"}
+        </button>
+      </div>
+      {showTable && renderTable()}
     </div>
   );
 };
