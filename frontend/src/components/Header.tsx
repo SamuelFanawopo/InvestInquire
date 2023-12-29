fe5fafbd9244b870abf6fc8876fbb154d0a0b174
Samuel Fanawopo <sfanawopo@gmail.com>
2023-12-29 07:55:21 +0000

    working on ticker search functionality

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -8,15 +8,14 @@ interface HeaderProps {
 const Header: React.FC<HeaderProps> = ({ name }) => {
   return (
     <header className="flex items-center my-4 mx-10">
-      <a href="/">
+      <a href="/" className="flex-grow-0">
         <h1 className="text-4xl font-bold">InvestInquire</h1>
       </a>
-      <div className="flex-grow"></div>{" "}
-      {/* This div pushes the following elements to the right */}
+      <div className="flex-grow"></div>
       <div className="flex items-center mr-14">
-        <TickerSearch />
+        <TickerSearch /> {/* Ensure this doesn't expand too much */}
       </div>
-      <a href="/login">
+      <a href="/login" className="flex-grow-0">
         <h2 className="text-xl font-bold">Welcome, {name}</h2>
       </a>
     </header>
--- frontend/src/components/TickerSearch.tsx
+++ frontend/src/components/TickerSearch.tsx
@@ -1,28 +1,76 @@
-// TickerSearch component
-import React, { useState } from "react";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const TickerSearch = () => {
   const [ticker, setTicker] = useState("");
+  const [suggestions, setSuggestions] = useState<string[]>([]); // Specify the type of suggestions
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
 
-  const handleSearch = () => {
-    // Implement your search logic here
+  useEffect(() => {
+    const delayDebounce = setTimeout(() => {
+      if (ticker) {
+        setIsLoading(true);
+        fetchSuggestions(ticker).then((data) => {
+          setSuggestions(data);
+          setIsLoading(false);
+        });
+      } else {
+        setSuggestions([]);
+      }
+    }, 500); // 500 ms delay for debouncing
+
+    return () => clearTimeout(delayDebounce);
+  }, [ticker]);
+
+  const handleSearch = (symbol: string) => {
+    navigate(`/company/${symbol}`);
+  };
+
+  const fetchSuggestions = async (query: string) => {
+    // Implement the logic to fetch suggestions
+    return ["AAPL", "MSFT", "GOOGL"]; // Dummy response for now
   };
 
   return (
-    <div className="flex">
-      <input
-        className="border-gray-300 border-2 rounded-lg py-2 px-4 w-full max-w-xs"
-        type="text"
-        value={ticker}
-        onChange={(e) => setTicker(e.target.value)}
-        placeholder="Enter ticker symbol"
-      />
-      <button
-        onClick={handleSearch}
-        className="bg-blue-500 hover:bg-blue-600 text-white rounded-lg py-2 px-6 transition duration-300 ml-2"
-      >
-        Search
-      </button>
+    <div className="relative w-full max-w-md">
+      {" "}
+      {/* Parent div with relative positioning */}
+      <div className="flex w-full">
+        <input
+          className="border-transparent rounded-t-lg py-2 px-4 w-full focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" // Updated classes
+          type="text"
+          value={ticker}
+          onChange={(e) => setTicker(e.target.value)}
+          placeholder="Enter ticker symbol"
+        />
+        <button
+          onClick={() => handleSearch(ticker)}
+          className="bg-blue-500 hover:bg-blue-700 text-white rounded-r-lg py-2 px-6 transition duration-300" // Updated classes
+        >
+          Search
+        </button>
+      </div>
+      {isLoading && (
+        <div className="absolute text-sm text-gray-600 w-full mt-2">
+          Loading...
+        </div>
+      )}
+      {!isLoading && suggestions.length > 0 && (
+        <ul className="absolute bg-white border border-transparent rounded-b-lg mt-1 w-full z-10 shadow-md">
+          {" "}
+          {/* Updated classes */}
+          {suggestions.map((symbol, index) => (
+            <li
+              key={index}
+              className="cursor-pointer hover:bg-gray-100 p-2 text-gray-800" // Updated classes
+              onClick={() => handleSearch(symbol)}
+            >
+              {symbol}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
