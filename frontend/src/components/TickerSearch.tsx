85fda011c01e0cf77e165c613fb3356495cb0976
Samuel Fanawopo <sfanawopo@gmail.com>
2023-12-31 17:41:24 +0000

    header bug

--- frontend/src/components/TickerSearch.tsx
+++ frontend/src/components/TickerSearch.tsx
@@ -1,46 +1,27 @@
-import { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const TickerSearch = () => {
-  const [ticker, setTicker] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
-  const [isTyping, setIsTyping] = useState(false);
+const TickerSearch: React.FC = () => {
+  const [ticker, setTicker] = useState<string>("");
   const navigate = useNavigate();
 
-  // Fixed data for demonstration purposes
-  const suggestions = ticker ? ["AAPL", "MSFT", "GOOGL"] : [];
+  const suggestions: string[] = ticker ? ["AAPL", "MSFT", "GOOGL"] : [];
 
-  useEffect(() => {
-    if (!ticker) {
-      setIsTyping(false);
-      return;
-    }
-
-    setIsLoading(true);
-    setIsTyping(true);
-
-    // Simulate an API call with a delay
-    const timeoutId = setTimeout(() => {
-      setIsLoading(false);
-    }, 500);
-
-    return () => clearTimeout(timeoutId);
-  }, [ticker]);
-
-  const handleSearch = (symbol: string) => {
-    if (symbol) {
-      navigate(`/company/${symbol}`);
-    }
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    console.log("Input Changed:", e.target.value); // Debug log
+    setTicker(e.target.value);
   };
 
-  const handleButtonClick = () => {
-    if (isTyping) {
-      setTicker("");
-      setIsTyping(false);
-    } else {
-      if (ticker) {
-        handleSearch(ticker);
-      }
+  const handleSuggestionClick = (symbol: string): void => {
+    console.log("Suggestion Clicked:", symbol); // Debug log
+    setTicker(symbol);
+    navigate(`/company/${symbol}`);
+  };
+
+  const handleButtonClick = (): void => {
+    console.log("Button Clicked:", ticker); // Debug log
+    if (ticker) {
+      navigate(`/company/${ticker}`);
     }
   };
 
@@ -51,30 +32,23 @@ const TickerSearch = () => {
           className="border-gray-300 border-2 rounded-lg py-2 px-4 w-full"
           type="text"
           value={ticker}
-          onChange={(e) => setTicker(e.target.value)}
+          onChange={handleInputChange}
           placeholder="Enter ticker symbol"
         />
         <button
           onClick={handleButtonClick}
           className="bg-blue-500 hover:bg-blue-700 text-white rounded-lg py-2 px-6 ml-2 transition duration-300"
         >
-          {isTyping ? "Cancel" : "Search"}
+          Search
         </button>
       </div>
-      {isLoading && (
-        <div className="text-sm text-gray-600 w-full mt-2">Loading...</div>
-      )}
-      {!isLoading && isTyping && (
+      {ticker && (
         <ul className="bg-white border border-gray-300 rounded-lg w-full z-10 shadow-md overflow-hidden">
           {suggestions.map((symbol, index) => (
             <li
               key={index}
               className="cursor-pointer hover:bg-gray-200 p-2"
-              onClick={() => {
-                setTicker(symbol);
-                setIsTyping(false);
-                handleSearch(symbol);
-              }}
+              onClick={() => handleSuggestionClick(symbol)}
             >
               {symbol}
             </li>
