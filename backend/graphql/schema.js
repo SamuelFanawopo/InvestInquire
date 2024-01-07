4918786c05b748cf01354e28611f2cc7e79b85d3
Samuel Fanawopo <sfanawopo@gmail.com>
2024-01-07 17:24:08 +0000

    companyscren schema

--- backend/graphql/schema.js
+++ backend/graphql/schema.js
@@ -43,6 +43,55 @@ export const typeDefs = gql`
     matchScore: Float
   }
 
+  type CompanyOverview {
+    Symbol: String
+    AssetType: String
+    Name: String
+    Description: String
+    CIK: String
+    Exchange: String
+    Currency: String
+    Country: String
+    Sector: String
+    Industry: String
+    Address: String
+    FiscalYearEnd: String
+    LatestQuarter: String
+    MarketCapitalization: Float
+    EBITDA: Float
+    PERatio: Float
+    PEGRatio: Float
+    BookValue: Float
+    DividendPerShare: Float
+    DividendYield: Float
+    EPS: Float
+    RevenuePerShareTTM: Float
+    ProfitMargin: Float
+    OperatingMarginTTM: Float
+    ReturnOnAssetsTTM: Float
+    ReturnOnEquityTTM: Float
+    RevenueTTM: Float
+    GrossProfitTTM: Float
+    DilutedEPSTTM: Float
+    QuarterlyEarningsGrowthYOY: Float
+    QuarterlyRevenueGrowthYOY: Float
+    AnalystTargetPrice: Float
+    TrailingPE: Float
+    ForwardPE: Float
+    PriceToSalesRatioTTM: Float
+    PriceToBookRatio: Float
+    EVToRevenue: Float
+    EVToEBITDA: Float
+    Beta: Float
+    WeekHigh52: Float
+    WeekLow52: Float
+    DayMovingAverage50: Float
+    DayMovingAverage200: Float
+    SharesOutstanding: Float
+    DividendDate: String
+    ExDividendDate: String
+  }
+
   type Query {
     marketNews(limit: Int): [NewsItem]
     profileNews(limit: Int): [ProfileNewsItem]
@@ -51,5 +100,6 @@ export const typeDefs = gql`
     topLosers: [MarketInfo]
     mostActivelyTraded: [MarketInfo]
     searchTickers(input: String!): [TickerMatch]
+    getCompanyOverview(symbol: String!): CompanyOverview
   }
 `;
