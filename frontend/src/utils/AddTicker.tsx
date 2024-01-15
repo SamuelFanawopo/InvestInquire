ca63d606a292172533713ae1cef95b1e6ac0501f
Samuel Fanawopo <sfanawopo@gmail.com>
2024-01-15 14:26:22 +0000

    added error checks and alert instead of navigation

--- frontend/src/utils/AddTicker.tsx
+++ frontend/src/utils/AddTicker.tsx
@@ -1,10 +1,8 @@
-import { doc, updateDoc, arrayUnion } from "firebase/firestore";
+import { doc, updateDoc, arrayUnion, getDoc } from "firebase/firestore";
 import { getFirestore } from "firebase/firestore";
-import { NavigateFunction } from "react-router-dom";
 
 export const addTickerToWatchlist = async (
   symbol: string,
-  navigate: NavigateFunction,
   authUser: { uid: string } | null, // Assuming authUser has at least a uid property
 ): Promise<void> => {
   const db = getFirestore();
@@ -13,12 +11,32 @@ export const addTickerToWatchlist = async (
     const userDocRef = doc(db, "users", authUser.uid);
 
     try {
-      await updateDoc(userDocRef, {
-        watchlist: arrayUnion(symbol),
-      });
-      navigate("/profile");
+      // Retrieve the current watchlist
+      const userDoc = await getDoc(userDocRef);
+      if (userDoc.exists()) {
+        const userData = userDoc.data();
+        const watchlist = userData.watchlist as string[];
+
+        // Check if the ticker already exists
+        if (watchlist.includes(symbol)) {
+          alert("Ticker already exists in your watchlist.");
+          return;
+        }
+
+        // Add ticker to watchlist
+        await updateDoc(userDocRef, {
+          watchlist: arrayUnion(symbol),
+        });
+        alert("Ticker added to your watchlist.");
+      } else {
+        // Handle case where user document doesn't exist
+        console.error("User document does not exist");
+      }
     } catch (error) {
       console.error("Error updating document: ", error);
     }
+  } else {
+    // Handle case where authUser or symbol is not available
+    alert("Please log in to add tickers to your watchlist.");
   }
 };
